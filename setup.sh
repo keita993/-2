8417f32bb1d59e7ae7e4bba6ce3ccb3bfe303c20
坂下　慶太 <keita993@yahoo.co.jp>
2025-04-30 19:11:17 +0900

    2

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+./configure --prefix=/usr
+make
+sudo make install
+cd ..
+rm -rf ta-lib-0.4.0-src.tar.gz ta-lib/ 
